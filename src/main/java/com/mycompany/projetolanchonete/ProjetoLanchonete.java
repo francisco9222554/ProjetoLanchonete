a03f1cf852fdeac245d80b062c1a6e7004106dce
francisco9222554 <francisco9222554@rede.ulbra.br>
2026-09-22 22:57:17 -0300

    finalizado

---
+++ src/main/java/com/mycompany/projetolanchonete/ProjetoLanchonete.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetolanchonete;
+
+/**
+ *
+ * @author aluno.saolucas
+ */
+public class ProjetoLanchonete {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
